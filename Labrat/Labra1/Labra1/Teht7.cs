9b1f9c0adfa18dbf0738dcc61f14a8fad3adfebe
Joeli Pikkarainen <Joeli Pikkarainen>
2017-09-06 16:09:23 +0300

    tehtävä 7 tehty

---
+++ Labrat/Labra1/Labra1/Teht7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra1
+{
+    class Teht7
+    {
+        public static void LeapYear()
+        {
+            int year;
+            bool isLeapYear;
+            isLeapYear = false;
+            Console.WriteLine("Vuosi luku:");
+            year = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(year % 4);
+            //Console.WriteLine(year % 100);
+           // Console.WriteLine(year % 400);
+            
+            if ((year % 4 == 0 && year % 100 != 0)||(year % 400 == 0))
+            {
+                isLeapYear = true;
+            }
+            else
+            {
+                isLeapYear = false;
+            }
+           
+            
+
+            Console.WriteLine(isLeapYear);
+        }
+    }
+}
