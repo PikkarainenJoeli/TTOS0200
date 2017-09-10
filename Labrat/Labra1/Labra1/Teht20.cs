9444ed78621f6c9f8302a4a55757e1225cf61ab0
Joeli Pikkarainen <Joeli Pikkarainen>
2017-09-11 01:29:08 +0300

    matopeli paranneltu

--- Labrat/Labra1/Labra1/Teht20.cs
+++ Labrat/Labra1/Labra1/Teht20.cs
@@ -37,12 +37,10 @@ namespace Labra1
                 for (int j = 0; j < area.GetLength(1); j++) // rivi sarake 1,2,3
                 {    
                     Console.Write(area[i, j]);
-                }
-                
+                }                
                 Console.WriteLine("");
             }
-            Console.WriteLine(dirString);
-
+           Console.WriteLine(dirString);
         }
 
 
@@ -50,24 +48,14 @@ namespace Labra1
         {
             List<int> OldY = new List<int>();
             List<int> OldX = new List<int>();
-                         /*
-                        OldY.Add(123);
-                        OldY.Add(312);
-                        Console.WriteLine("y at 0"+ OldY.ElementAt(1));
-                         Console.ReadLine();    
 
-                         OldY.Add(1);
-                        Console.WriteLine("capa");
-                        Console.WriteLine(OldX.Count);
-                        Console.WriteLine(OldY.Count);
-                        Console.ReadLine();
-                        */
 
             int currentVert;
             int currentHor;
             int vert;
             int hor;
-            int [,]oldSpaces = new int[200,2];
+            int [,]oldSpaces = new int[2000,2];
+           
 
             string dirString;
             Random rnd = new Random();          
@@ -86,28 +74,28 @@ namespace Labra1
              timer.Enabled = true;
              timer.Start();
 
-                                     // riviä, saraketta
-                                    //vert hor
-            char[,] area = new char[10, 20];
+            // riviä, saraketta
+            //vert hor
+            const int rows = 10;
+            const int columns = 25;
+       
+            char[,] area = new char[rows, columns];
             
-
-            Console.WriteLine(area.GetLength(0));
-            Console.WriteLine("Here");
 
             for (int i = 0;i < area.GetLength(0); i++)// tehdään sarake 1,2,3
             {
                         for (int j = 0; j < area.GetLength(1); j++) // rivi sarake 1,2,3
                             {
-                                area[i, j] = 'x';
-                            Console.Write(area[0, j]);
+                                area[i, j] = '.';
+                            //Console.Write(area[0, j]);
 
                             }
-                Console.WriteLine("");
+               // Console.WriteLine("");
             }
 
             Stopwatch t = new Stopwatch();
-            Console.WriteLine("START");
-            Console.ReadLine();
+            Console.WriteLine("Press any to Start!");
+            //Console.ReadKey();
             int round = 0;
             int score = 0;
             int y = 0;
@@ -117,79 +105,112 @@ namespace Labra1
             //empty needed
             OldY.Add(0);
             OldX.Add(0);
+            int NewScoreVert = rnd.Next(0, rows);
+            int NewScoreHort = rnd.Next(0, columns);
 
-
+            bool paused;
+            paused = false;
             while (Alive)
             {
-                //round++;
-                //Console.WriteLine(round);
 
                 t.Start();
                 TimeSpan ts = t.Elapsed;
-                TimeElapsed = ts.Seconds;
+                TimeElapsed = ts.Milliseconds;
      
-                if (GetAsyncKeyState(38) != 0 && dirString != "Down")
-                {
-                    vert = -1;
-                    hor = 0;
-                    dirString = "Up";
-                   // Console.WriteLine("UP");
-                }
-                else if (GetAsyncKeyState(40) != 0 && dirString != "Up")
-                {
-                    vert = 1;
-                    hor = 0;
-                    dirString = "Down";
-                    // Console.WriteLine("DOWN");
-                }
+                        if (GetAsyncKeyState(38) != 0 && dirString != "Down")
+                        {
+                            vert = -1;
+                            hor = 0;
+                            dirString = "Up";
+                           // Console.WriteLine("UP");
+                        }
+                        else if (GetAsyncKeyState(40) != 0 && dirString != "Up")
+                        {
+                            vert = 1;
+                            hor = 0;
+                            dirString = "Down";
+                            // Console.WriteLine("DOWN");
+                        }
 
-                else if (GetAsyncKeyState(39) != 0 && dirString != "Left")
-                {
-                    vert = 0;
-                    hor = 1;
-                    dirString = "Right";
-                    //Console.WriteLine("RIGHT");
-                }
+                        else if (GetAsyncKeyState(39) != 0 && dirString != "Left")
+                        {
+                            vert = 0;
+                            hor = 1;
+                            dirString = "Right";
+                            //Console.WriteLine("RIGHT");
+                        }
                
-                else if (GetAsyncKeyState(37) != 0 && dirString != "Right")
-                {
-                    vert = 0;
-                    hor = -1;
-                    dirString = "Left";
-                  //  Console.WriteLine("LEFT");
-                }
+                        else if (GetAsyncKeyState(37) != 0 && dirString != "Right")
+                        {
+                            vert = 0;
+                            hor = -1;
+                            dirString = "Left";
+                          //Console.WriteLine("LEFT");
+                        }
 
-                if (TimeElapsed > 0.1)//_________________________________________________________________________________________________
+                        else if (GetAsyncKeyState(32) !=0 && paused == false) {;Console.ReadKey();}
+               
+                if (TimeElapsed > 150)// new game tick every x milliseconds.
                 {
+                    
                     t.Reset();
                     TimeElapsed = 0;
 
-                    if(area[currentVert + vert, currentHor + hor] == 'o')
+                    if( (currentVert + vert < 0 || currentVert + vert > rows -1) || (currentHor + hor < 0 || currentHor + hor > columns-1))
                     {
-                        score++;
-                        int NewScoreVert = rnd.Next(0, 10);
-                        int NewScoreHort = rnd.Next(0, 20);
-                        area[NewScoreVert, NewScoreVert] = 'o';
+                        Console.Write("Game Over");Console.Read();
+                        Labra1.Menu();
                     }
-                    //drew current head space
+
+                            if (area[currentVert + vert, currentHor + hor] == 'o')
+                            {
+                                score++;
+
+                        //make new score location and check that it free
+                        NewScoreVert = rnd.Next(0, 10);
+                        NewScoreHort = rnd.Next(0, 20);
+                        if (area[NewScoreVert, NewScoreHort] != '■')
+                        {
+                            area[NewScoreVert, NewScoreVert] = 'o';
+                        }
+                        else
+                        {
+                            while (area[NewScoreVert, NewScoreHort] == '■')
+                            {
+                                NewScoreVert = rnd.Next(0, 10);
+                                NewScoreHort = rnd.Next(0, 20);
+                                area[NewScoreVert, NewScoreVert] = 'o';
+                            }
+
+                        }
+                                
+                                //Console.WriteLine("next score at function: " + NewScoreVert + " " + NewScoreHort +" "+ area[NewScoreVert, NewScoreHort]);
+                                //Console.ReadLine();
+                    }
+
+                    if (area[currentVert + vert, currentHor + hor] == '■')
+                    {
+                        Console.WriteLine("GAME OVER");
+                        Console.ReadLine();
+                        Labra1.Menu();
+                    }
+
+                    //drew new head space and remove too much latest tail at index y - score
                     area[currentVert + vert,currentHor + hor] = '■';
 
                     currentVert = currentVert + vert;
                     currentHor = currentHor + hor;
 
-
                     oldSpaces[y, 0] = currentVert;
                     oldSpaces[y, 1] = currentHor;
-
                    OldY.Add(currentVert);
                    OldX.Add(currentHor);
 
-                    area[OldY.ElementAt(y-score), OldX.ElementAt(y-score)] = 'x';
-                  
-
-                  
-
-             
+                    if (area[OldY.ElementAt(y - score), OldX.ElementAt(y - score)] != 'o')
+                    {
+                        area[OldY.ElementAt(y - score), OldX.ElementAt(y - score)] = '.';
+                    }
+                    else { Console.WriteLine("Its 'o' "); Console.ReadLine(); }
 
                     display(area,dirString);
            
@@ -198,7 +219,8 @@ namespace Labra1
                     Console.Write(" oldX:"+OldX[OldX.Count-1]);
                     Console.WriteLine(" Score:" +score);
 
-                    Console.Write("To delete: Y:" + OldY.ElementAt(y)+" X:" + OldX.ElementAt(y)+" Y:" + y);
+                    Console.WriteLine("next score at: " + NewScoreVert +" " + NewScoreHort);
+                    //Console.Write("To delete: Y:" + OldY.ElementAt(y)+" X:" + OldX.ElementAt(y)+" Y:" + y);
                     //Console.ReadLine();
                     y++;
                 }
